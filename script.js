d93c86f5355f85b272d7ee16a9fdbfb0129d6947
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-01 19:57:35 +0530

    added all the click Event listeners

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-const COLORS = [
-    "red",
-    "green",
-    "cyan",
-    "black",
-    "voilet",
-    "aqua",
-    "blue",
-    "yellow",
-    "purple",
-    "orange",
-    "lightgreen",
-    "lemon",
+const COLORS = ["red","green","cyan","black","violet","aqua","blue","yellow","purple","orange","lightgreen","lemon",
+];
+
+const SHAPES = [
+  "square",
+  "triangle",
+  "circle",
+  "rectangle",
+  "diamond",
 ];
 
 let changeColorBtn = document.getElementById("change-color");
 let changeShapeBtn = document.getElementById("change-shape");
+let changeShapeColorBtn = document.getElementById("change-shape-color");
 let box = document.getElementById("box");
 let innerBox = document.getElementById("inner-box");
 
 // Click Events on btns
-changeColorBtn.addEventListener('click', colorChange);
-// changeShapeBtn.addEventListener('click', shapeChange);
+changeColorBtn.addEventListener("click", colorChange);
+changeShapeBtn.addEventListener("click", shapeChange);
+changeShapeColorBtn.addEventListener("click", shapeColorChange);
 
-// Event functions
 function colorChange() {
-    let randomColorIndex = Math.floor(Math.random() * COLORS.length - 1);
-    console.log(randomColorIndex);
-    box.style.backgroundColor = COLORS[randomColorIndex];
+  let randomIndex = Math.floor(Math.random() * COLORS.length);
+  box.style.backgroundColor = COLORS[randomIndex];
+}
+
+function shapeChange() {
+    let randomIndex = Math.floor(Math.random() * SHAPES.length);
+    innerBox.className = "shape " + SHAPES[randomIndex];
+}
+
+function shapeColorChange() {
+    let randomIndex = Math.floor(Math.random() * COLORS.length);
+    innerBox.style.backgroundColor = COLORS[randomIndex];
+  
 }
